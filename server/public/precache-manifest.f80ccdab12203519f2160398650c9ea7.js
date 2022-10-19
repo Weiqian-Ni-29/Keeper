f1163805b129fb1fa7954885e2f65f572cf69525
Weiqian-Ni-29 <101934090+Weiqian-Ni-29@users.noreply.github.com>
2022-10-19 16:33:37 +0800

    Add files via upload

---
+++ server/public/precache-manifest.f80ccdab12203519f2160398650c9ea7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6e2719ad8fdb611a719",
+    "url": "/static/js/main.e6e2719a.chunk.js"
+  },
+  {
+    "revision": "bd5eccd96b90dd352871",
+    "url": "/static/js/1.bd5eccd9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "540a1b93fc5bc3ca76c3ceb2b85295c2",
+    "url": "/index.html"
+  }
+];
